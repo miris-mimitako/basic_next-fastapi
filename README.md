6bcaf212fada670a88a0c041bcb2c15a1f7c2984
miris-mimitako <miris.mikitako@gmail.com>
2023-02-19 22:32:49 +0900

    add: styele

--- README.md
+++ README.md
@@ -6,8 +6,13 @@ Next.js and FastAPI
 You install following application
 
 * Node.js - https://nodejs.org/ja/download/
-* Python
+* Python - 3.11 or latest
+* SCSS - 
 
 ### Fast API
 
 https://fastapi.tiangolo.com/
+
+## Information
+
+This application is basic web component. You must modify this applciation along with your things.
--- front/next_base/components/header.jsx
+++ front/next_base/components/header.jsx
@@ -1,21 +1,40 @@
 import Link from 'next/link';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
-
-export default function Header() {
+export default function ButtonAppBar() {
   return (
-    <ul>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/about" >About</Link>
-      </li>
-      <li>
-      <div>
-      <Button variant="contained">Hello World</Button>
-    </div>
-      </li>
-    </ul>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <div class ="header_link--wrapper">
+                <div class ="header_link--content">
+                    <Link href="/">Home</Link>
+                </div>
+                <div class = "header_link--content">
+                    <Link href="/about">About</Link>
+                </div>
+            </div>
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
