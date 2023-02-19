bf114d33d7c6993138851fc36e9986ebd5270e7b
miris-mimitako <miris.mikitako@gmail.com>
2023-02-19 14:02:38 +0900

    add

--- front/next_base/pages/read_sql.js
+++ front/next_base/pages/read_sql.js
@@ -1,47 +1,48 @@
 // This is sample of generate table
 function ReadSQL({ posts }) {
-    console.log(posts);
-  
-    const obj = JSON.parse(posts);
-    const obj_keys = Object.keys(obj[0]);
-  
-    // console.log(obj_keys)
-    {
-      obj.map((post) => {
-        post;
-      });
-    }
-    return (
-      <>
-        <table>
-          <thead>
+  console.log(posts);
+
+  const obj = JSON.parse(posts);
+  const obj_keys = Object.keys(obj[0]);
+
+  // console.log(obj_keys)
+  {
+    obj.map((post) => {
+      post;
+    });
+  }
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
             {obj_keys.map((obj_key) => (
-              <th>{obj_key}</th>
+              <th key={obj_key}>{obj_key}</th>
             ))}
-          </thead>
-          <tbody>
-            {obj.map((post) => (
-              <tr>
-                <td>{post.number}</td>
-                <td>{post.sample}</td>
-                <td>{post.value}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </>
-    );
-  }
-  
-  export async function getStaticProps() {
-    const res = await fetch("http://127.0.0.1:8000/read_csv");
-    const posts = await res.json();
-    return {
-      props: {
-        posts,
-      },
-    };
-  }
-  
-  export default ReadSQL;
-  
+          </tr>
+        </thead>
+        <tbody>
+          {obj.map((post) => (
+            <tr key={post.id}>
+              <td>{post.id}</td>
+              <td>{post.name}</td>
+              <td>{post.age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("http://127.0.0.1:8000/read_sql");
+  const posts = await res.json();
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default ReadSQL;
