572f80d0152b7067aacb7f1e4c65d32fce1172e4
miris-mimitako <miris.mikitako@gmail.com>
2023-02-19 12:27:37 +0900

    add new database

---
+++ api/modules/readsql.py
@@ -0,0 +1,14 @@
+import sqlalchemy
+import pandas as pd
+
+class SQLread:
+    def __init__(self) -> None:
+        
+        
+        pass
+
+    def main(self):
+        pass
+
+if __name__=="__main__":
+    SR = SQLread()
